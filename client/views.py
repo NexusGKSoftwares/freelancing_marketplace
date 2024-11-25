b2ac3d7767810a2fd2ac71881b9fd81972a2540e
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-25 16:32:48 +0300

    "Added client login view with authentication and login functionality"

--- client/views.py
+++ client/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def client_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None and user.is_staff is False:  # To distinguish from admin users
+            login(request, user)
+            return redirect('client_dashboard')
+        else:
+            messages.error(request, "Invalid credentials!")
+    return render(request, 'client/login.html')
