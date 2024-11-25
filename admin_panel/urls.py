c254282f36b1af5041dd38d4a75a00648281c8ce
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-25 16:00:28 +0300

    "Removed duplicate URL patterns from admin_panel, client, and freelancer urls.py files"

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -18,8 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('client/', include('client.urls')),
-    path('freelancer/', include('freelancer.urls')),
-    path('admin_panel/', include('admin_panel.urls')),
+
 ]
