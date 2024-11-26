c785dea8a29771943786642bd385e0506b6ecdfd
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-26 22:42:33 +0300

    "Added UserSettings model and updated system_settings.html template"

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
@@ -9,3 +11,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+class UserSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    language = models.CharField(max_length=50, default='en')
+    timezone = models.CharField(max_length=50, default='UTC')
+    notifications_enabled = models.BooleanField(default=True)
+    dark_mode = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.user.username}'s settings"
