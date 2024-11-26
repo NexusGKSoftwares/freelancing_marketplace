8aa79ded1aa9f88a67cd82b88855646e049c95de
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-26 21:55:21 +0300

    "Updated forms.py to import Project model and modified forms.cpython-310.pyc"

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Project
 
 class AddUserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput, label="Password")
