3a5f9a8fb6677b12e3dd86c50608cc9f1eb9697e
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-27 15:35:31 +0300

    added index.html

--- freelancer/views.py
+++ freelancer/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, 'index.html')
+    else:
+        return HttpResponseRedirect(reverse('Portal:home'))
+    
 def freelancer_register(request):
     if request.method == 'POST':
         username = request.POST['username']
