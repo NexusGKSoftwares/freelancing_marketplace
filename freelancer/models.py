db97df2cbe41bdb679db39fdfe236990aa2f2f62
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-28 09:10:15 +0300

    "Added SkillProfile model and updated Profile model's __str__ method"

--- freelancer/models.py
+++ freelancer/models.py
@@ -8,6 +8,7 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
@@ -16,6 +17,10 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     skills = models.ManyToManyField(Skill, related_name="profiles", blank=True)
 
+
     def __str__(self):
         return f'{self.user.username} Profile'
     
+class SkillProfile(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
