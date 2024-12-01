5f99c6a73a2d4408a5cf3591277ba4989988aa11
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-01 13:57:51 +0300

    "Renamed 'dashboard/' URL pattern to 'client_dashboard/' in client/urls.py"

--- client/urls.py
+++ client/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.client_dashboard, name='client_dashboard'),
+    path('client_dashboard/', views.client_dashboard, name='client_dashboard'),
     path('post-job/', views.post_job, name='post_job'),
     path('edit-job/<int:job_id>/', views.edit_job, name='edit_job'),
     path('messages/', views.messages_view, name='messages'),
