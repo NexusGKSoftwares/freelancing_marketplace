3fae6be6f7bfe07bfe5bd2f22216f14d0122b067
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-01 16:20:10 +0300

    "Updated client URL patterns: renamed 'jobs' to 'job' in job detail URL, added edit and delete job URLs, and removed old edit job URL."

--- client/urls.py
+++ client/urls.py
@@ -5,10 +5,11 @@ urlpatterns = [
     path('client_dashboard/', views.client_dashboard, name='client_dashboard'),
     path('jobs/', views.job_list, name='job_list'), 
     path('jobs/filter/', views.filter_jobs, name='filter_jobs'),  
-    path('jobs/<int:job_id>/', views.job_detail, name='job_detail'),
+    path('job/<int:job_id>/', views.job_detail, name='job_detail'),
+    path('job/<int:job_id>/edit/', views.edit_job, name='edit_job'),
+    path('job/<int:job_id>/delete/', views.delete_job, name='delete_job'),
     path('post-job/', views.post_job, name='post_job'),
     path('preview-job/', views.preview_and_post_job, name='preview_and_post_job'),
-    path('edit-job/<int:job_id>/', views.edit_job, name='edit_job'),
     path('messages/', views.messages_view, name='messages'),
     path('notifications/', views.notifications_view, name='notifications'),
 ]
