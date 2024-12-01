e50c290a4b2f455085eb73ed2959c168686e79fc
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-01 14:20:31 +0300

    "Updated client dashboard template and URL pattern for jobs"

--- client/urls.py
+++ client/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('client_dashboard/', views.client_dashboard, name='client_dashboard'),
-    path('jobs/', views.jobs, name='jobs'),
+    path('jobs_list/', views.jobs, name='jobs'),
     path('post-job/', views.post_job, name='post_job'),
     path('edit-job/<int:job_id>/', views.edit_job, name='edit_job'),
     path('messages/', views.messages_view, name='messages'),
