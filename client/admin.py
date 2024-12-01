46289c8a1201feb2f3b23a70cad7bbe2dfcc14da
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-01 14:28:06 +0300

    "Commented out admin registration and JobAdmin class in client/admin.py"

--- client/admin.py
+++ client/admin.py
@@ -1,9 +1,9 @@
-from admin_panel import admin
-from client.models import Job
+# from admin_panel import admin
+# from client.models import Job
 
 
-@admin.register(Job)
-class JobAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'type', 'status', 'min_budget', 'max_budget', 'posted_on')
-    list_filter = ('category', 'type', 'status', 'posted_on')
-    search_fields = ('title', 'description')
+# @admin.register(Job)
+# class JobAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'category', 'type', 'status', 'min_budget', 'max_budget', 'posted_on')
+#     list_filter = ('category', 'type', 'status', 'posted_on')
+#     search_fields = ('title', 'description')
