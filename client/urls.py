b1a13b0b5ccb4cea1cf5ef1e196941654e2e235a
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-01 14:21:04 +0300

    "Updated client/urls.py to add new URL patterns for job list, filter, and detail views, and modified existing URL pattern for jobs list."

--- client/urls.py
+++ client/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('client_dashboard/', views.client_dashboard, name='client_dashboard'),
-    path('jobs_list/', views.jobs, name='jobs'),
+    path('jobs/', views.job_list, name='job_list'), 
+    path('jobs/filter/', views.filter_jobs, name='filter_jobs'),  
+    path('jobs/<int:job_id>/', views.job_detail, name='job_detail'),
     path('post-job/', views.post_job, name='post_job'),
     path('edit-job/<int:job_id>/', views.edit_job, name='edit_job'),
     path('messages/', views.messages_view, name='messages'),
