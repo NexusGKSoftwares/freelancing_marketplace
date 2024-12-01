6dbed3cee46450e8b493bf72419a3b898685e019
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-01 18:09:04 +0300

    "Removed and added URL patterns in freelancer/urls.py"

--- freelancer/urls.py
+++ freelancer/urls.py
@@ -1,31 +1,15 @@
-"""
-URL configuration for freelancing_marketplace project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-
-from freelancer import views 
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.freelancer_register, name='freelancer_register'),
-    path('login/', views.freelancer_login, name='freelancer_login'),
-    path('dashboard/', views.dashboard, name='freelancer_dashboard'),
-    path('logout/', views.freelancer_logout, name='freelancer_logout'),
-    path('profile/', views.profile, name='profile'),  # View user profile
-    path('profile/edit/', views.edit_profile, name='edit_profile'),  # Edit user profile
-    path('profile/search/', views.profile_search, name='profile_search'),  # Search for users
+    path('dashboard/', views.freelancer_dashboard, name='freelancer_dashboard'),
+    path('profile/edit/', views.freelancer_edit_profile, name='freelancer_edit_profile'),
+    path('payments/', views.freelancer_payment_overview, name='freelancer_payment_overview'),
+    path('active-jobs/', views.freelancer_ongoing_jobs, name='freelancer_ongoing_jobs'),
+    path('available-jobs/', views.freelancer_available_jobs, name='freelancer_available_jobs'),
+    path('notifications/', views.freelancer_notifications, name='freelancer_notifications'),
+    path('feedback/', views.freelancer_feedback, name='freelancer_feedback'),
+    path('job/<int:job_id>/', views.freelancer_job_details, name='freelancer_job_details'),
+    path('payment-history/', views.freelancer_payment_history, name='freelancer_payment_history'),
+    path('job-history/', views.freelancer_job_history, name='freelancer_job_history'),
 ]
