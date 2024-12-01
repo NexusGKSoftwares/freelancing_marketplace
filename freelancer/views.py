fdcfd4179808cfad74d22e2d20691fe7fd42adae
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-01 18:21:00 +0300

    "Added two imports to views.py and updated corresponding pycache file."

--- freelancer/views.py
+++ freelancer/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Freelancer, Job, Notification, Feedback
 from django.contrib.auth.decorators import login_required
 
