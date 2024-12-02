b98c234a9d96d8a975dde515c4245543a1a562a3
Bomano <bokem658@gmail.com>
2024-12-02 23:30:45 +0300

    "Added CustomUser model with role field, updated models.py and corresponding __pycache__ file"

--- freelancer/models.py
+++ freelancer/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.auth.models import AbstractUser
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('freelancer', 'Freelancer'),
+        ('client', 'Client'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
 class Freelancer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
