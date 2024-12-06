516b06f05ba6a6b90e836a4e2318c510e05157c4
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-06 13:23:47 +0300

    done

--- FreelancingIIITS/IIITSFreelancingPortal/static/extnavjs.js
+++
@@ -1,6 +0,0 @@
-const trigger = document.querySelector('.mobile-trigger');
-const navbarLinks = document.querySelector('nav ul');
-trigger.addEventListener('click', ()=> {
-  trigger.classList.toggle('active');
-	navbarLinks.classList.toggle('active');
- });
