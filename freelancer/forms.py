6d0712aafc3dabb9b20005873ee1dc0559aa9816
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-10 07:25:55 +0300

    `Added ProfilePictureForm to freelancer/forms.py and updated forms.cpython-313.pyc`

--- freelancer/forms.py
+++ freelancer/forms.py
@@ -5,3 +5,5 @@ class ModelForm(forms.ModelForm):
     class Meta:
         model = ModelName
         fields = ('__all__')
+class ProfilePictureForm(forms.Form):
+    picture = forms.ImageField(label="Upload Profile Picture")
