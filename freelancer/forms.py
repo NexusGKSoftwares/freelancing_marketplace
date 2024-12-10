c9a506e3c6fbeabc7436eb92cd92a10e68691f75
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-10 07:33:15 +0300

    `Added profile picture upload feature and updated related models and signals`

--- freelancer/forms.py
+++ freelancer/forms.py
@@ -1,9 +1,4 @@
 from django import forms
-from .models import ModelName
 
-class ModelForm(forms.ModelForm):
-    class Meta:
-        model = ModelName
-        fields = ('__all__')
 class ProfilePictureForm(forms.Form):
     picture = forms.ImageField(label="Upload Profile Picture")
--- freelancer/models.py
+++ freelancer/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Freelancer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='freelancer_profile')
     total_earnings = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    
+    picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
