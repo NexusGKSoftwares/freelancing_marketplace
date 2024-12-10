ae588247dbc2a1d262cb6e08fdcc2cce7bcb1f85
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-10 13:48:58 +0300

    `Update settings.py to use SQLite database instead of MySQL`

--- freelancing_marketplace/settings.py
+++ freelancing_marketplace/settings.py
@@ -77,23 +77,29 @@ WSGI_APPLICATION = 'freelancing_marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'freelance',  # Replace with the name of your database in phpMyAdmin
+#         'USER': 'root',                # Default MySQL root user
+#         'PASSWORD': '',                # No password
+#         'HOST': 'localhost',           # MySQL server is running locally
+#         'PORT': '3306',                # Default MySQL port
+#         'OPTIONS': {
+#             'sql_mode': 'traditional', # Optional: Adjust MySQL SQL modes
+#             'charset': 'utf8mb4',      # Ensure UTF-8 compatibility
+#         },
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'freelance',  # Replace with your MySQL database name
-        'USER': 'root',  # Replace with your MySQL username
-        'PASSWORD': '',   # Replace with your MySQL password
-        'HOST': 'localhost',           # Replace with your database host, if not on localhost
-        'PORT': '3306',                # Default MySQL port (change if needed)
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
