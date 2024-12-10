497c03f2f4c5a1b1c6a8f21507c5385d396c2864
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-10 13:31:15 +0300

    `Updated database configuration to use MySQL instead of SQLite`

--- freelancing_marketplace/settings.py
+++ freelancing_marketplace/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'freelancing_marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freelance',  # Replace with your MySQL database name
+        'USER': 'root',  # Replace with your MySQL username
+        'PASSWORD': '',   # Replace with your MySQL password
+        'HOST': 'localhost',           # Replace with your database host, if not on localhost
+        'PORT': '3306',                # Default MySQL port (change if needed)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
