1901b2ff140cd359f11e0fe91a51d8e0272ca109
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-10 07:40:41 +0300

    `Updated freelancer app configuration and signals to import from correct locations`

--- freelancer/apps.py
+++ freelancer/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
 
-class YourAppNameConfig(AppConfig):
+class YourAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'freelancer'
 
     def ready(self):
-        import freelancer.signals
+        import freelancer.signals 
