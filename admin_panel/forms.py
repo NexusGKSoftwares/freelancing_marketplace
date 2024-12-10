d8d02a894649a9e72c332c8b74d8041a2ccc2a45
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-10 08:04:05 +0300

    `Added StaticPageForm and customized fields in ProjectForm`

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Project
+from .models import Project, StaticPage
 
 class AddUserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput, label="Password")
@@ -12,3 +12,13 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description', 'status', 'start_date', 'end_date']
+
+
+class StaticPageForm(forms.ModelForm):
+    class Meta:
+        model = StaticPage
+        fields = ['title', 'content']
+
+    # Customizing form fields if needed
+    title = forms.CharField(max_length=200, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 6}), required=True)
