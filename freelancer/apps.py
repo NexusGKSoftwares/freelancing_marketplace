602c2532850158d04f52d719b293b7454fb45386
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-10 07:41:00 +0300

    "Rename AppConfig class from 'YourAppConfig' to 'FreelancerConfig' in freelancer/apps.py"

--- freelancer/apps.py
+++ freelancer/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
 
-class YourAppConfig(AppConfig):
+class FreelancerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'freelancer'
 
     def ready(self):
-        import freelancer.signals 
+        import freelancer.signals  # Replace 'freelancer' with the name of your app
